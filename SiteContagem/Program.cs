aaf9f7582ecc22436c52a9400552c06ee02ad1ff
Renato Groffe <renato.groffe@yahoo.com.br>
2022-10-24 17:20:05 -0300

    .NET 7 Release Candidate 2 + ASP.NET Core + Razor Pages + Application Insights + Docker + Linux
    .NET 7 Release Candidate 2 + ASP.NET Core + Razor Pages + Application Insights + Docker + Linux

---
+++ SiteContagem/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddApplicationInsightsTelemetry(options =>
+{
+    options.ConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
